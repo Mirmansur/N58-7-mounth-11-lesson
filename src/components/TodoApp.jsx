41c5ff658730d73984dfcb891a3a662f1158535b
Mirmansur <147687431+Mirmansur@users.noreply.github.com>
2024-08-08 21:17:25 +0500

    last

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import useStore from "./store";
+import axios from "axios";
+
+const TodoApp = () => {
+  const { todos, fetchTodos, addTodo, toggleTodo, deleteTodo } = useStore();
+
+  useEffect(() => {
+    fetchTodos();
+  }, [fetchTodos]);
+
+  const handleAddTodo = () => {
+    const newTodo = { id: Date.now(), title: "New Todo", completed: false };
+    addTodo(newTodo);
+    axios.post("http://localhost:3000/todos", newTodo);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.title}
+            </span>
+            <button onClick={() => toggleTodo(todo.id)}>Toggle</button>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoApp;
